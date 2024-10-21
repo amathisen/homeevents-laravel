af7f2dfa848ef40b9f526641a59bcbfe1a4eec4e
Aaron Mathisen <35702793+amathisen@users.noreply.github.com>
2024-10-20 17:47:34 -0700

    Spacing

--- resources/views/activity_object.blade.php
+++ resources/views/activity_object.blade.php
@@ -3,8 +3,11 @@
 <center><h1>{{$activity_object->name}}</h1></center>
 <br>
 <b>Games Played:</b> {{count($event_activities_results_objects)}}<br>
-<b>Average Placement:</b> {{(array_sum($results_values) / count($event_activities_results_objects))}}<br>
+<b>Average Placement:</b> {{(array_sum($results_values) / count($event_activities_results_objects))}}
+<br>
+
 <br><hr><br>
+
 <table>
 @foreach($events as $this_event)
     <tr>
