7442010df54cf566052631d2d843444f09c8fe03
Aaron Mathisen <35702793+amathisen@users.noreply.github.com>
2024-10-25 17:35:17 -0700

    Activity Object details updates
    Add user list, opponents results, and average placements to activity object details view page

--- app/Http/Controllers/ActivityObjectController.php
+++ app/Http/Controllers/ActivityObjectController.php
@@ -13,19 +13,71 @@ class ActivityObjectController extends Controller
         $page_title = "Activity Object - " . $activity_object->name;
         $event_activities_results_objects = $activity_object->get_referring_results('event_activities_results_objects');
         $results_values = array();
+        $results_values_opponents = array();
         $events = array();
-        
+        $users = array();
+        $scores = array();
+        $scores_opponents = array();
         
         foreach($event_activities_results_objects as $this_event_activities_results_object) {
             $event_activities_results = $this_event_activities_results_object->get_associated_result('event_activities_results');
             $event_activities = $event_activities_results->get_associated_result('event_activities');
             $event = $event_activities->get_associated_result('event');
+            $user = new Blank('users',$event_activities_results->users_id);
             $event->event_activities_name = $event_activities->name;
             $event->event_activities_results_result_value = $event_activities_results->result_value;
+            $opponents_results = new Blank('event_activities_results');
+            $opponents_results = $opponents_results->get_all(limit_by:'event_activities_id = ' . $event_activities->id . ' AND users_id != ' . $event_activities_results->users_id);
             array_push($events,$event);
             array_push($results_values,$event_activities_results->result_value);
+            $opponents_results['user_results'] = $event_activities_results;
+            array_push($results_values_opponents,$opponents_results);
+            
+            if(!in_array($user,$users))
+                array_push($users,$user);
+            if(!isset($scores[$event_activities_results->result_value]))
+                $scores[$event_activities_results->result_value] = 1;
+            else
+                $scores[$event_activities_results->result_value] += 1;
         }
         
-        return view('activity_object',compact("page_title","activity_object","event_activities_results_objects","events","results_values"));
+        foreach($results_values_opponents as $these_results) {
+            $user_results = $these_results['user_results'];
+            unset($these_results['user_results']);
+            foreach($these_results as $this_result) {
+                $tmp_event_activities_results_obj = $this_result->get_referring_results('event_activities_results_objects')[0];
+                $tmp_activity_obj = $tmp_event_activities_results_obj->get_associated_result('activity_object');
+                if(!isset($scores_opponents[$tmp_activity_obj->id])) {
+                    $scores_opponents[$tmp_activity_obj->id]['title'] = $tmp_activity_obj->name;
+                    $scores_opponents[$tmp_activity_obj->id][$this_result->result_value] = 1;
+                } else {
+                    if(!isset($scores_opponents[$tmp_activity_obj->id][$this_result->result_value]))
+                        $scores_opponents[$tmp_activity_obj->id][$this_result->result_value] = 1;
+                    else
+                        $scores_opponents[$tmp_activity_obj->id][$this_result->result_value] += 1;
+                }
+                if(!isset($scores_opponents[$tmp_activity_obj->id . '_user'][$user_results->result_value])) {
+                    $scores_opponents[$tmp_activity_obj->id . '_user'][$user_results->result_value] = 1;
+                } else {
+                    $scores_opponents[$tmp_activity_obj->id . '_user'][$user_results->result_value] += 1;
+                }
+                ksort($scores_opponents[$tmp_activity_obj->id]);
+            }  
+        }
+
+
+        foreach($scores_opponents as $key => $these_scores) {
+            $total_placement = 0;
+            $total_games = 0;
+            foreach($these_scores as $key2 => $value) {
+                if($key2 == 'title') continue;
+                $total_placement = $total_placement + ($key2*$value);
+                $total_games += $value;
+            }
+            $these_scores['average_placement'] = ($total_placement / $total_games);
+            $scores_opponents[$key] = $these_scores;
+        }
+
+        return view('activity_object',compact("page_title","activity_object","event_activities_results_objects","events","results_values","users","scores","scores_opponents"));
     }
 }
--- resources/views/activity_object.blade.php
+++ resources/views/activity_object.blade.php
@@ -1,10 +1,48 @@
 @include('header')
 
-<center><h1>{{$activity_object->name}}</h1></center>
+<center>
+<h1>{{$activity_object->name}}</h1>
+<h2>By @foreach($users as $this_user) {!!$this_user->get_href()!!} @endforeach</h2>
+</center>
 <br>
 <b>Games Played:</b> {{count($event_activities_results_objects)}}<br>
 <b>Average Placement:</b> {{(array_sum($results_values) / count($event_activities_results_objects))}}
 <br>
+<table>
+@foreach($scores as $key => $value)
+    <tr>
+        <td class="indent title">{{$key}} Place</td>
+        <td>{{$value}}</td>
+    </tr>
+@endforeach
+</table>
+<br>
+@foreach($scores_opponents as $these_scores)
+<table>
+    <tr>
+        <td class="title" colspan="2">
+        @if(isset($these_scores['title']))
+            {{$these_scores['title']}}
+        @else
+            {{$activity_object->name}}
+        @endif
+        @if(isset($these_scores['average_placement']))
+            - Average Placement: {!!$these_scores['average_placement']!!}
+        @endif
+        </td>
+    </tr>
+    @foreach($these_scores as $key => $value)
+        @continue($key == 'title' || $key == 'average_placement')
+        <tr>
+            <td class="indent">{{$key}} Place</td>
+            <td>{{$value}} time(s)</td>
+        </tr>
+    @endforeach
+</table>
+@if(!isset($these_scores['title']))
+    <br>
+@endif
+@endforeach
 
 <br><hr><br>
 
