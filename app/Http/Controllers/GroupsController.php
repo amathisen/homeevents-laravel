6088c3935eed6addf788e40df4b601ee319fe27a
Aaron Mathisen <35702793+amathisen@users.noreply.github.com>
2024-10-31 10:07:47 -0700

    Groups detail view - implement new logic
    Update groups detail view to use new 'include' logic for efficiency

--- app/Http/Controllers/GroupsController.php
+++ app/Http/Controllers/GroupsController.php
@@ -8,12 +8,9 @@ use Illuminate\Http\Request;
 class GroupsController extends Controller
 {
     public function index($groups_id) {
-        $group = new Blank('groups',$groups_id);
+        $group = new Blank('groups',$groups_id,include_referrals:'users_groups,event',sort_by:'event.date,users_groups.users_id,users_groups.roles_groups_id');
         $page_title = "Group - " . $group->name;
 
-        $users_groups = get_all_blank('users_groups',limit_by:'groups_id = ' . $group->id,sort_by:'users_id,roles_groups_id');
-        $events = get_all_blank('event',limit_by:'groups_id = ' . $group->id,sort_by:'date');
-
-        return view('groups',compact("page_title","group","users_groups","events"));
+        return view('groups',compact("page_title","group"));
     }
 }
--- resources/views/groups.blade.php
+++ resources/views/groups.blade.php
@@ -10,21 +10,21 @@
         <td class="title">User</td>
         <td class="title">Role</td>
     </tr>
-@foreach($users_groups as $this_group)
+@foreach($group->get_ref_values('users_groups') as $this_group)
     <tr>
-        <td>{!!get_href_by_type_and_id('users',$this_group->users_id);!!}</td>
-        <td>{{$this_group->get_fk_values('roles_groups_id',array('name'))['name']}}</td>
+        <td>{!!get_href_by_type_and_id('users',$this_group['users_id']);!!}</td>
+        <td>{{get_value_by_type_and_id('roles_groups',$this_group['roles_groups_id'],'name')['name']}}</td>
     </tr>
 @endforeach
 </table>
 <br><hr><br>
 <table>
 <tr><td class="title" colspan="3">Events</td></tr>
-@foreach($events as $this_event)
+@foreach($group->get_ref_values('event') as $this_event)
     <tr>
-        <td class="indent">{!!get_href_by_type_and_id('location',$this_event->location_id);!!}</td>
-        <td>{!!$this_event->get_href();!!}</td>
-        <td>{{date_display($this_event->date);}}</td>
+        <td class="indent">{!!get_href_by_type_and_id('location',$this_event['location_id']);!!}</td>
+        <td>{!!get_href_by_type_and_id('event',$this_event['id']);!!}</td>
+        <td>{{date_display($this_event['date']);}}</td>
     </tr>
 @endforeach
 </table>
